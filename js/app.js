f7376847e68b95030d8d5f73273c3a0c3d417837
chinesebaloni <berty.ruan@gmail.com>
2015-07-04 11:35:04 -0400

    Early push to revert back to init project state

--- js/app.js
+++ js/app.js
@@ -3,8 +3,9 @@
  * attendance record if one is not found
  * within localStorage.
  */
-(function() {
-    if (!localStorage.attendance) {
+/*
+function(){
+        if (!localStorage.attendance) {
         console.log('Creating attendance records...');
         function getRandom() {
             return (Math.random() >= 0.5);
@@ -24,14 +25,96 @@
 
         localStorage.attendance = JSON.stringify(attendance);
     }
-}());
+    */
+
+$(function(){
+    octopus.init();
+});
+
+var model = {
+    days: 12,
+    studentNames : [
+        {
+            name: "Slappy the Frog"
+
+        },
+        {
+            name: "Lily the Lizard"
+        },
+        {
+            name: "Paulrus the Walrus"
+        },
+        {
+            name: "Gregory the Goat"
+        },
+        {
+            name: "Adam the Anaconda"
+        }
+    ],
+    init: function(){
+        if(!localStorage.attendance)
+        {
+            localStorage.attendance = JSON.stringify([]);
+        }
+    },
+    add: function(obj){
+        var data = JSON.parse(localStorage.attendance);
+        data.push(obj);
+        localStorage.attendance = JSON.stringify(data);
+    },
+    getAllAttendance: function(){
+        return JSON.parse(localStorage.attendance);
+    }
+};
+
+var octopus = {
+    init: function(){
+        view.renderTable();
+        view.renderCheckBoxes();
+        view.renderMissed();
+    },
+    getAttendance: function(){
+        return model.getAllAttendance();
+    },
+    getDays: function(){
+        return model.days;
+    }
+
+};
+
+var view = {
+    init: function(){
+        this.allMissed = $('tbody .missed-col');
+        this.allCheckboxes = $('tbody input');
+
+    },
+    renderTable: function(){
+        for(var i=octopus.getDays();i>0;i--)
+        {
+            $("<th>"+i+"</th>").insertAfter('thead .name-col');
+        }
+    },
+    renderCheckBoxes: function(){
+        console.log("hi");
+        var htmlStr = "";
+        $.each(octopus.getAttendance(), function(index, element){
+            console.log(index,element);
+        });
+        for(var i=0;i<octopus.getDays();i++)
+        {
+            htmlStr += '<td class="attend-col"><input type="checkbox"></td>';
+        }
+        return htmlStr;
+    },
+    renderMissed: function(){
+
+    }
+};
 
 
 /* STUDENT APPLICATION */
-$(function() {
-    var attendance = JSON.parse(localStorage.attendance),
-        $allMissed = $('tbody .missed-col'),
-        $allCheckboxes = $('tbody input');
+function hello() {
+        
 
     // Count a student's missed days
     function countMissing() {
@@ -61,7 +144,7 @@ $(function() {
     });
 
     // When a checkbox is clicked, update localStorage
-    $allCheckboxes.on('click', function() {
+    $allCheckboxes.on('0click', function() {
         var studentRows = $('tbody .student'),
             newAttendance = {};
 
@@ -81,4 +164,4 @@ $(function() {
     });
 
     countMissing();
-}());
+};
